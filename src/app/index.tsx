25a09f54a3262a82d3c73b93867c95926d9f2def
whdjh <wngns9807@gmail.com>
2025-08-28 01:36:07 +0900

    chore: 메인페이지 함수 제거후 직접 선언

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,14 +4,6 @@ import { useRouter } from 'expo-router'
 export default function HomeScreen() {
   const router = useRouter()
 
-  const goToSignin = () => {
-    router.push('/signin' as any);
-  }
-
-  const goToSignup = () => {
-    router.push('/signup' as any);
-  }
-
   return (
     <View className="flex-1 justify-center items-center bg-black px-6">
       <Text className="text-3xl font-bold mb-6 text-center text-white">
@@ -19,13 +11,13 @@ export default function HomeScreen() {
       </Text>
       <View className='flex flex-row gap-3'>
         <TouchableOpacity
-          onPress={goToSignin}
+          onPress={() => router.push('/signin' as any)}
           className="bg-zinc-700 px-6 py-3 rounded-2xl"
         >
           <Text className="text-white text-lg font-semibold text-center">로그인</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={goToSignup}
+          onPress={() => router.push('/signup' as any)}
           className="bg-zinc-700 px-6 py-3 rounded-2xl"
         >
           <Text className="text-white text-lg font-semibold text-center">회원가입</Text>
